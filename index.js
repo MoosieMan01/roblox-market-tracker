61b7d4054060d6cc7b3cb63bb7a08ef8580d044d
MoosieMan01 <113198456+MoosieMan01@users.noreply.github.com>
2025-12-20 22:36:48 +0100

    First update.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const admin = require("firestore_admin");
-admin.InitializeApp({ credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT))})
+admin.InitializeApp({ credential: admin.credential.cert(JSON.parse(process.env.FIRESTORE_JSON))})
+
+const db = admin.firestore();
+
+async function run() {
+  const docRef = db.collection("game2").doc(); // auto-generated doc ID
+
+  // Set the document data
+  await docRef.set({
+    timestamp: new Date()
+  });
+
+  console.log(`Created collection '${collectionName}' with a document.`);
+}
+
+run().catch(console.error);
