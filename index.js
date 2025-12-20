eec7b20b45943351f3f684735e74a5a477473470
MoosieMan01 <113198456+MoosieMan01@users.noreply.github.com>
2025-12-20 22:41:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-admin.InitializeApp({ credential: admin.credential.cert(JSON.parse(process.env.FIRESTORE_JSON))})
+admin.InitializeApp({ credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT))})
 
 const db = admin.firestore();
 
