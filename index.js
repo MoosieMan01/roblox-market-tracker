161372c5f58a4fb0ab55cb075d5e0aae38858263
MoosieMan01 <113198456+MoosieMan01@users.noreply.github.com>
2025-12-23 00:06:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,43 +17,21 @@ async function run() {
   );
   const json = await res.json();
 
-  const topGames = (json.collection?.items || json.games || []).map(g => ({
+  const topPlaces = (json.collection?.items || json.games || []).map(g => ({
     placeId: g.rootPlaceId,
     playing: g.playerCount
   }));
 
   const dateStr = new Date().toISOString().split("T")[0];
-  for (const game of topGames) {
-
-    const universeRes = await fetch(
-      `https://apis.roblox.com/universes/v1/places/${game.placeId}/universe`
-    );
-    const universeData = await universeRes.json();
-
-    console.log(JSON.stringify(universeData, null, 2));
-    if (!universeData.universeId) continue;
-    const universeId = universeData.universeId;
-
-
-    const gameRes = await fetch(
-      `https://games.roblox.com/v1/games?universeIds=${universeId}`
-    );
-    const gameData = await gameRes.json();
-
-    if (!gameData.data || !gameData.data[0]) continue;
-
-    const gameId = gameData.data[0].id;
-    const players = gameData.data[0].playing;
-
+  for (const place of topPlaces) {
     await db
-      .collection("games")
-      .doc(gameId.toString())
+      .collection("places")
+      .doc(place.placeId.toString())
       .collection("history")
       .doc(dateStr)
       .set({
-        players
+        players: place.playing
       });
-
   }
 }
 
