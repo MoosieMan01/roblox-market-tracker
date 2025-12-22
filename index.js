c3b9fb380dbed64c38ead1013051aa33411d7da7
MoosieMan01 <113198456+MoosieMan01@users.noreply.github.com>
2025-12-22 23:50:07 +0100

    Updated index to 100 games

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
 import admin from "firebase-admin";
 
-admin.initializeApp({ credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT))})
+admin.initializeApp({
+  credential: admin.credential.cert(
+    JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  )
+});
 
 const db = admin.firestore();
 
 async function run() {
-  const placeId = 79546208627805
-  const universeRes = await fetch(`https://apis.roblox.com/universes/v1/places/${placeId}/universe`);
-  const universeData = await universeRes.json();
-  const universeId = universeData.universeId;
-  
-  const gameRes = await fetch(`https://games.roblox.com/v1/games?universeIds=${universeId}`);
-  const gameData = await gameRes.json();
-  
+  const sessionId = crypto.randomUUID();
+  const sortId = "top-playing-now";
 
-  const gameId = gameData.data && gameData.data[0] ? gameData.data[0].id : 0;
-  const players = gameData.data && gameData.data[0] ? gameData.data[0].playing : 0;
+  const res = await fetch(
+    `https://apis.roblox.com/explore-api/v1/get-sort-content?sortId=${sortId}&limit=100&cursor=0&sessionId=${sessionId}`
+  );
+  const json = await res.json();
+
+  const topGames = (json.collection?.items || json.games || []).map(g => ({
+    placeId: g.rootPlaceId,
+    playing: g.playerCount
+  }));
 
   const dateStr = new Date().toISOString().split("T")[0];
 
-  const historyRef = db.collection("games").doc(gameId.toString()).collection("history").doc(dateStr); 
-    await historyRef.set({
-    players: players,
-  });
+  for (const game of topGames) {
+
+    const universeRes = await fetch(
+      `https://apis.roblox.com/universes/v1/places/${game.placeId}/universe`
+    );
+    const universeData = await universeRes.json();
+
+    if (!universeData.universeId) continue;
+    const universeId = universeData.universeId;
+
+
+    const gameRes = await fetch(
+      `https://games.roblox.com/v1/games?universeIds=${universeId}`
+    );
+    const gameData = await gameRes.json();
+
+    if (!gameData.data || !gameData.data[0]) continue;
+
+    const gameId = gameData.data[0].id;
+    const players = gameData.data[0].playing;
+
+    await db
+      .collection("games")
+      .doc(gameId.toString())
+      .collection("history")
+      .doc(dateStr)
+      .set({
+        players
+      });
+
+    console.log(`Updated ${gameId}: ${players}`);
+  }
 }
 
 run().catch(console.error);
